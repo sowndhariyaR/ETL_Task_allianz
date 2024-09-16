6196014ec8e9df540b605e4a7807b55de09a1ca0
sowndhariyaR <149314924+sowndhariyaR@users.noreply.github.com>
2024-09-16 15:40:45 +0530

    Update etl_program.py

--- etl_program.py
+++ etl_program.py
@@ -4,14 +4,9 @@ from transformer import transform_data_to_excel
 from zipping import zip_html_files
 
 async def main():
-    # Scrape all pages
     html_contents = await scrape_team_stats()
-
-    # Transform and save data to Excel
     transform_data_to_excel(html_contents)
-
-    # Zip HTML files
-    zip_html_files(html_files=html_contents)  # Pass html_contents as positional argument
+    zip_html_files(html_files=html_contents)  
 
 if __name__ == "__main__":
     asyncio.run(main())
